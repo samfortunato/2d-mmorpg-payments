20a606126b7559075eebdc4694dd02ee30685f00
Samuel Fortunato <samfortunato@gmail.com>
2021-05-03 20:49:49 -0400

    Make payment server work with test payments

--- index.js
+++ index.js
@@ -1,14 +1,49 @@
 const { fastify } = require('fastify');
+const fastifyCors = require('fastify-cors');
+const { Stripe } = require('stripe');
 
 const server = fastify({ logger: true });
+const stripe = new Stripe('sk_test_qmOUTLgYKZEOTk74EyRGvd9D', { apiVersion: '2020-08-27' });
 
-server.get('/', async (req, res) => {
-  return { test: 'penis' };
+server.register(fastifyCors, {
+  origin: [
+    'http://localhost:8080',
+    'http://superatomic.net',
+  ],
+});
+
+server.post('/create-checkout-session', async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    line_items: [{
+      price_data: {
+        currency: 'usd',
+        product_data: {
+          name: '2D MMORPG Subscription',
+        },
+        unit_amount: 500,
+      },
+      quantity: 1,
+    }],
+    mode: 'payment',
+    // success_url: 'http://localhost:8080', // local
+    // cancel_url: 'http://localhost:8080', // local
+    success_url: 'http://superatomic.net', // prod lol
+    cancel_url: 'http://superatomic.net', // prod lol
+  });
+
+  return { id: session.id };
+});
+
+server.get('/health', async (_, res) => {
+  res
+    .code(200)
+    .send();
 });
 
 const start = async () => {
   try {
-    await server.listen(8082);
+    await server.listen(8082, '0.0.0.0');
   } catch (err) {
     server.log.error(err);
 
@@ -17,3 +52,23 @@ const start = async () => {
 };
 
 start();
+
+// const { fastify } = require('fastify');
+
+// const server = fastify();
+
+// server.get('/health', async (_, res) => {
+//   res
+//     .code(200)
+//     .send();
+// });
+
+// const start = async () => {
+//   try {
+//     await server.listen(8082, '0.0.0.0');
+//   } catch (err) {
+//     process.exit(1);
+//   }
+// };
+
+// start();
